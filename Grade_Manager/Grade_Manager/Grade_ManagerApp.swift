3a7466982eb53c1425d21c9e58c7e82d9973ecf6
jkarstrom <44649059+jkarstrom@users.noreply.github.com>
2021-04-19 14:49:36 -0700

    Adding Xcode Project

---
+++ Grade_Manager/Grade_Manager/Grade_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grade_ManagerApp.swift
+//  Grade_Manager
+//
+//  Created by Joan Karstrom on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Grade_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
